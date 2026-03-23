4e8f42def7962e495f7dc538f79acc07e431dd19
rhellhbn <164168446+rhellhbn@users.noreply.github.com>
2026-03-23 16:58:13 +0700

    Rahel

--- data.sql
+++ data.sql
@@ -1,28 +1,33 @@
 CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                                     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(100) NOT NULL,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
     photo VARCHAR(255) NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
-);
+    );
 
 CREATE TABLE IF NOT EXISTS refresh_tokens (
-     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-     user_id UUID NOT NULL,
-     refresh_token TEXT NOT NULL,
-     auth_token TEXT NOT NULL,
-     created_at TIMESTAMP NOT NULL
-);
+                                              id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL,
+    refresh_token TEXT NOT NULL,
+    auth_token TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL
+    );
 
-CREATE TABLE IF NOT EXISTS todos (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+CREATE TABLE IF NOT EXISTS books (
+                                     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL,
     title VARCHAR(100) NOT NULL,
+    author VARCHAR(100) NOT NULL,
     description TEXT NOT NULL,
-    is_done BOOLEAN NOT NULL,
+    genre VARCHAR(50) NOT NULL DEFAULT 'Umum',
+    isbn VARCHAR(20) NULL,
+    publisher VARCHAR(100) NULL,
+    year INT NULL,
+    is_read BOOLEAN NOT NULL DEFAULT FALSE,
     cover TEXT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
-);
+    );
